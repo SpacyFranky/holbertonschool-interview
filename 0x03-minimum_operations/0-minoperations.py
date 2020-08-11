8eb1f501165892f0b05b604ea0f67dc1e4642c42
spacyfranky <bigaziz@outlook.com>
2020-08-11 14:26:16 +0000

    minimum operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+def minOperations(n):
+    """ Calculates the fewest number of operations needed to result
+        in exactly n H characters in the file
+    """
+    if (n < 2):
+        return 0
+    if (type(n) is not int):
+        return 0
+    
+    n_op = 0
+    copy_all = 1
+    h = 1
+    while (n > h):
+        if (n % h == 0):
+            copy_all = h
+            h *= 2
+            n_op += 2
+        else:
+            h += copy_all
+            n_op += 1
+    
+    return n_op
