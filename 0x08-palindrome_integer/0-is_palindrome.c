89c93e758d31303f5f521f2bb37346fefadf0742
spacyfranky <bigaziz@outlook.com>
2020-10-17 15:17:41 +0000

    betty fix

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -7,22 +7,22 @@
 * is_palindrome -  checks whether or not a given unsigned
 * integer is a palindrome
 * @n: unsigned long int
-* Retun: 1 if n is a palindrome, and 0 otherwise.
+* Return: 1 if n is a palindrome, and 0 otherwise.
 */
 int is_palindrome(unsigned long n)
-{       
-        int i, j;
-        char buffer[sizeof(unsigned long) * 8 + 1];
-        (void)(n);
+{
+	int i, j;
+	char buffer[sizeof(unsigned long) * 8 + 1];
+	(void)(n);
 
-        sprintf(buffer, "%lu", n);
+	sprintf(buffer, "%lu", n);
 
-        j = strlen(buffer) - 1;
-        for (i = 0; i <= j; i++)
-        {
-                if (buffer[i] != buffer[j])
-                        return (0);
-                j--;
-        }
-        return (1);
+	j = strlen(buffer) - 1;
+	for (i = 0; i <= j; i++)
+	{
+		if (buffer[i] != buffer[j])
+			return (0);
+		j--;
+	}
+	return (1);
 }
