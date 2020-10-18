493eda487f9252a81de7ed6c74b413766e78cd4f
spacyfranky <bigaziz@outlook.com>
2020-10-18 13:23:24 +0000

    final edge case handeling

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,30 +13,30 @@ try:
         try:
             file_size = line.split(" ")[8]
             int(file_size)
+            file_size = file_size[:-1]
         except Exception:
             pass
-        file_size = file_size[:-1]
         size += int(file_size)
 
         try:
             status_code = line.split(" ")[7]
             int(status_code)
+            list_of_status_code.append(int(status_code))
+            list_of_status_code.sort()
+            dictionary = dict((i, list_of_status_code.count(i))
+                              for i in list_of_status_code)
+            dictionary_items = dictionary.items()
+            sorted_items = sorted(dictionary_items)
+
+            i = i + 1
+            if (i == 10):
+                print("File size: {:d}".format(size))
+                for k, v in sorted_items:
+                    print("{:d}: {:d}".format(k, v))
+                i = 0
+
         except Exception:
             pass
-        list_of_status_code.append(int(status_code))
-        list_of_status_code.sort()
-
-        dictionary = dict((i, list_of_status_code.count(i))
-                          for i in list_of_status_code)
-        dictionary_items = dictionary.items()
-        sorted_items = sorted(dictionary_items)
-
-        i = i + 1
-        if (i == 10):
-            print("File size: {:d}".format(size))
-            for k, v in sorted_items:
-                print("{:d}: {:d}".format(k, v))
-            i = 0
 
 except KeyboardInterrupt:
     pass
