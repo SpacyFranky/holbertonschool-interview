3858ac8533b75c7f93e390292a8abd5e30f188d7
spacyfranky <bigaziz@outlook.com>
2020-10-18 12:12:38 +0000

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,36 +2,34 @@
 import sys
 
 i = 0
-j = 0
 list_of_filesize = []
 list_of_status_code = []
 s = 0
-while j < 4:
-    while i < 10:
-        try:
-            line = sys.stdin.readline()
-        except KeyboardInterrupt:
-            break
 
-        if not line:
-            break
+while i < 10:
+    try:
+        line = sys.stdin.readline()
+    except KeyboardInterrupt:
+        break
 
-        file_size = line.split(" ")[8]
-        file_size = file_size[:-1]
-        list_of_filesize.append(int(file_size))
+    if not line:
+        break
 
-        status_code = line.split(" ")[7]
-        list_of_status_code.append(int(status_code))
-        list_of_status_code.sort()
-        dictionary = dict((i, list_of_status_code.count(i))
-                          for i in list_of_status_code)
-        dictionary_items = dictionary.items()
-        sorted_items = sorted(dictionary_items)
+    file_size = line.split(" ")[8]
+    file_size = file_size[:-1]
+    list_of_filesize.append(int(file_size))
 
-        i = i + 1
+    status_code = line.split(" ")[7]
+    list_of_status_code.append(int(status_code))
+    list_of_status_code.sort()
+    dictionary = dict((i, list_of_status_code.count(i))
+                        for i in list_of_status_code)
+    dictionary_items = dictionary.items()
+    sorted_items = sorted(dictionary_items)
 
-    s += sum(list_of_filesize)
-    print("File size: {:d}".format(s))
-    for k, v in sorted_items:
-        print("{}: {}".format(k, v))
-    j = j + 1
+    i = i + 1
+
+s += sum(list_of_filesize)
+print("File size: {:d}".format(s))
+for k, v in sorted_items:
+    print("{}: {}".format(k, v))
