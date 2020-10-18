53bda862e698cb9e1208d904432b0d80596f7db0
spacyfranky <bigaziz@outlook.com>
2020-10-18 13:37:06 +0000

    fixing the last edge case

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,35 +10,28 @@ sorted_items = {}
 
 try:
     for line in sys.stdin:
-        try:
-            line_split = line.split(" ")
-            file_size = line_split[-1]
-            int(file_size)
-            size += file_size
-        except Exception:
-            pass
+        line_split = line.split(" ")
+        file_size = line_split[-1]
+        
+        size += int(file_size)
 
-        try:
-            status_code = line_split[-2]
-            int(status_code)
-            list_of_status_code.append(int(status_code))
-            list_of_status_code.sort()
-            dictionary = dict((i, list_of_status_code.count(i))
-                              for i in list_of_status_code)
-            dictionary_items = dictionary.items()
-            sorted_items = sorted(dictionary_items)
+        status_code = line_split[-2]
+        list_of_status_code.append(int(status_code))
+        list_of_status_code.sort()
+        dictionary = dict((i, list_of_status_code.count(i))
+                          for i in list_of_status_code)
+        dictionary_items = dictionary.items()
+        sorted_items = sorted(dictionary_items)
 
-            i = i + 1
-            if (i == 10):
-                print("File size: {:d}".format(size))
-                for k, v in sorted_items:
-                    print("{:d}: {:d}".format(k, v))
-                i = 0
+        i = i + 1
+        if (i == 10):
+            print("File size: {:d}".format(size))
+            for k, v in sorted_items:
+                print("{:d}: {:d}".format(k, v))
+            i = 0
 
-        except Exception:
-            pass
 
-except KeyboardInterrupt:
+except Exception:
     pass
 
 finally:
