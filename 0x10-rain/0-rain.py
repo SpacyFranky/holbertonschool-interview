4e808690f4e8b3f6d6fdbd2208e35a53eb3b8060
spacyfranky <bigaziz@outlook.com>
2020-11-23 14:42:03 +0000

    Calculating the retained rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Calculates how much retained water """
+
+def rain(walls):
+    """ Returns the total amount of rainwater retained """
+
+    if len(walls) == 0:
+        return 0
+
+    s = 0
+    for i in range(1, len(walls) - 1):
+        s += walls[i]
+    return s
