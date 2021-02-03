95b0d9038170621e7e3c6e7d85786fc111a8c4a3
spacyfranky <bigaziz@outlook.com>
2021-02-03 13:24:05 +0000

    getting Star Wars characters name

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,18 +1,29 @@
 #!/usr/bin/node
-const process = require('process');
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(url, function (err, response, body) {
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+
+request(url, { json: true }, (err, res, body) => {
   if (err) {
     return console.log(err);
   }
-  const list = JSON.parse(body).characters;
-  for (let i = 0; i < list.length; i++) {
-    request.get(list[i], function (errList, responseList, bodyList) {
-      if (errList) {
-        return console.log(errList);
+
+  const names = [];
+
+  body.characters.forEach(element => {
+    const id = element.split('/')[5];
+    request(element, { json: true }, (err, res, body) => {
+      if (err) {
+        return console.log(err);
       }
-      return console.log(JSON.parse(bodyList).name);
+      names[id] = body.name;
     });
-  }
+  });
+
+  setTimeout(function () {
+    body.characters.forEach(element => {
+      const id = element.split('/')[5];
+      console.log(names[id]);
+    });
+  }, 2000);
 });
