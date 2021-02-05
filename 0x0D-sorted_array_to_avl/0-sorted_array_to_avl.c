8d1113025c7c290e01c2efff9015c6d9607bda8b
spacyfranky <bigaziz@outlook.com>
2021-02-05 16:16:21 +0000

    fixed comments

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,18 +10,18 @@
 */
 avl_t *new(avl_t *parent, int value)
 {
-    avl_t *node;
+	avl_t *node;
 
-    node = malloc(sizeof(avl_t));
-    if (node == NULL)
-        return (NULL);
+	node = malloc(sizeof(avl_t));
+	if (node == NULL)
+		return (NULL);
 
-    node->parent = parent;
-    node->right = NULL;
-    node->left = NULL;
-    node->n = value;
+	node->parent = parent;
+	node->right = NULL;
+	node->left = NULL;
+	node->n = value;
 
-    return (node);
+	return (node);
 }
 
 /**
@@ -29,23 +29,24 @@ avl_t *new(avl_t *parent, int value)
 * @array: array of integers
 * @first: first element
 * @last: last element
+* @parent: parent of node
 * Return: a pointer to the root node of the created AVL tree,
 * or NULL failure
 */
 avl_t *avl(int *array, int first, int last, avl_t *parent)
 {
-    avl_t *tree;
-    int mid;
+	avl_t *tree;
+	int mid;
 
-    if (first > last)
-        return (NULL);
+	if (first > last)
+		return (NULL);
 
-    mid = (first + last) / 2;
-    tree = new(parent, array[mid]);
-    tree->left = avl(array, first, mid - 1, tree);
-    tree->right = avl(array, mid + 1, last, tree);
+	mid = (first + last) / 2;
+	tree = new(parent, array[mid]);
+	tree->left = avl(array, first, mid - 1, tree);
+	tree->right = avl(array, mid + 1, last, tree);
 
-    return (tree);
+	return (tree);
 }
 
 /**
@@ -57,13 +58,13 @@ avl_t *avl(int *array, int first, int last, avl_t *parent)
 */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *tree, *parent;
+	avl_t *tree, *parent;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    parent = NULL;
-    tree = avl(array, 0, size - 1, parent);
+	parent = NULL;
+	tree = avl(array, 0, size - 1, parent);
 
-    return (tree);
+	return (tree);
 }
