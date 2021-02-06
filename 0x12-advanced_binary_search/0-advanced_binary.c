d378de350ea86bd8f74e8ab02ca657525feebb9f
spacyfranky <bigaziz@outlook.com>
2021-02-06 00:06:38 +0000

    Advanced binary search

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,71 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+* print_array - prints array
+* @array: array of integers
+* @first: first element
+* @last: last element
+*/
+void print_array(int *array, size_t start, size_t end)
+{
+	size_t i = start;
+
+	printf("Searching in array: ");
+	while (i <= end)
+	{
+		printf("%d", array[i]);
+		if (i != end)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+* binary_search - searches for a value in a sorted array
+* of integers
+* @array: sorted array
+* @first: first element
+* @last: last element
+* @value: searched value
+* Return: index where value is located, or -1 if it's not present
+*/
+int binary_search(int *array, size_t start, size_t end, int value)
+{
+	size_t mid;
+
+	print_array(array, start, end);
+
+	mid = (start + end) / 2;
+
+	if (mid >= end)
+		return (-1);
+
+	else if (value == array[start])
+		return (start);
+
+	else if (value <= array[mid - 1])
+		return (binary_search(array, start, mid, value));
+
+	else if (value >= array[mid + 1])
+		return (binary_search(array, mid + 1, end, value));
+
+	return (-1);
+}
+
+
+/**
+* advanced_binary - calls the binary search function
+* of integers
+* @array: sorted array
+* @size: size of array
+* @value: searched value
+* Return: index where value is located, or -1 if it's not present
+*/
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (size == 0)
+		return (-1);
+	return (binary_search(array, 0, size - 1, value));	
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _BINARY_SEARCH_
+#define _BINARY_SEARCH_
+
+#include <stdlib.h>
+
+int binary_search(int *array, size_t start, size_t end, int value);
+int advanced_binary(int *array, size_t size, int value);
+void print_array(int *array, size_t start, size_t end);
+int _putchar(char c);
+
+#endif /* _BINARY_SEARCH_ */
