573c041de9c60bfbb6b3ff242080320c6a79742e
spacyfranky <bigaziz@outlook.com>
2021-07-08 20:42:17 +0000

    redix sort

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include "sort.h"
+
+
+/**
+* get_max - finds the largest number in the array
+* @array: array of integers
+* @size: size of the array
+* Return: largest number in the array
+*/
+int get_max(int *array, int size) {
+
+	int i;
+	int max = array[0];
+
+	for (i = 1; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+
+	return (max);
+}
+
+/**
+* counting_sort - counts the elements
+* @array: array of integers
+* @size: size of the array
+* @pos: position in the array
+*/
+void counting_sort(int *array, int size, int pos) {
+
+	int i;
+	int max = (array[0] / pos) % 10;
+	int *result, *count;
+
+	result = malloc((sizeof(int) * size) + 1);
+	if (result == NULL)
+		return;
+
+	count = malloc((sizeof(int) * max) + 1);
+	if (count == NULL)
+		return;
+
+	for (i = 1; i < size; i++)
+	{
+		if (((array[i] / pos) % 10) > max)
+			max = array[i];
+	}
+
+	for (i = 0; i < max; ++i)
+		count[i] = 0;
+
+
+	for (i = 0; i < size; i++)
+		count[(array[i] / pos) % 10]++;
+    
+
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		result[count[(array[i] / pos) % 10] - 1] = array[i];
+		count[(array[i] / pos) % 10]--;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = result[i];
+
+
+	free(result);
+}
+
+
+/**
+* radix_sort -  sorts an array of integers in ascending order
+* using the Radix sort algorithm
+* @array: array of integers
+* @size: size of array
+*/
+void radix_sort(int *array, size_t size)
+{
+
+	int pos, max;
+
+	max = get_max(array, size);
+	for (pos = 1; max / pos > 0; pos *= 10)
+	{
+		counting_sort(array, size, pos);
+		print_array(array, size);
+	}
+
+}
---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef _RADIX_
+#define _RADIX_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+
+#endif /* _RADIX_ */
