d4d2f83ffd222bc6e1d4c778268155799a608def
spacyfranky <bigaziz@outlook.com>
2021-07-15 22:36:51 +0000

    adding edge cases

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -7,16 +7,16 @@ def island_perimeter(grid):
     land = 0
     if len(grid) <= 2:
         return land
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i]) - 1):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 land += 4
-                if grid[i][j-1] == 1:
+                if grid[i][j-1] == 1 and j - 1 >= 0:
                     land -= 1
-                if grid[i][j+1] == 1:
+                if grid[i][j+1] == 1 and j + 1 <= len(grid[j]):
                     land -= 1
-                if grid[i+1][j] == 1:
+                if grid[i+1][j] == 1 and i + 1 <= len(grid):
                     land -= 1
-                if grid[i-1][j] == 1:
+                if grid[i-1][j] == 1 and i - 1 >= 0:
                     land -= 1
     return land
