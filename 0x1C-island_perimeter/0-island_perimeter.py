e9c424ccf09b99d9fef70cb0cb5959e16130b1aa
spacyfranky <bigaziz@outlook.com>
2021-07-15 22:05:39 +0000

    changed tabs to spaces for pep8 validation

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -3,18 +3,18 @@
 
 
 def island_perimeter(grid):
-	""" returns the perimeter of the island described in grid """
-	land = 0
-	for i in range(1, len(grid) - 1):
-		for j in range(1, len(grid[i]) - 1):
-			if grid[i][j] == 1:
-				land += 4
-				if grid[i][j-1] == 1:
-					land -= 1
-				if grid[i][j+1] == 1:
-					land -= 1
-				if grid[i+1][j] == 1:
-					land -= 1
-				if grid[i-1][j] == 1:
-					land -= 1
-	return land
+    """ returns the perimeter of the island described in grid """
+    land = 0
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[i]) - 1):
+            if grid[i][j] == 1:
+                land += 4
+                if grid[i][j-1] == 1:
+                    land -= 1
+                if grid[i][j+1] == 1:
+                    land -= 1
+                if grid[i+1][j] == 1:
+                    land -= 1
+                if grid[i-1][j] == 1:
+                    land -= 1
+    return land
